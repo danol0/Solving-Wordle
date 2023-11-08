a9d31bb0edf24cc5378facc1f328852081b447f5
danol0 <116915585+danol0@users.noreply.github.com>
2023-11-08 10:57:22 +0000

    updated helper function loop

--- wordle_helper.py
+++ wordle_helper.py
@@ -7,7 +7,7 @@ def wordleHelper() :
     attempt = 0
     answerList = wf.loadAnswers()
 
-    while attempt < 5 :
+    while attempt < 6 :
 
         #precomputed best first guess to save time
         if attempt == 0 :
@@ -18,7 +18,13 @@ def wordleHelper() :
         else :
             guess = input("Enter what you guessed: ")
             pattern = list(map(int, input("Enter the pattern as a number with no spaces: ")))
+
+            if pattern == [2,2,2,2,2] :
+                return
+            
             answerList = wf.filterAnswers(guess,pattern,answerList)
             bestGuess = wf.returnGuess(answerList)
             print("Computed guess: ", bestGuess)
             attempt += 1
+
+wordleHelper()
